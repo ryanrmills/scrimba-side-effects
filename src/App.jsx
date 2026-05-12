99a85fa4142d892667ffb759697c71a1c6ed4a92
ryanrmills <ryanroymills@gmail.com>
2026-05-11 21:40:04 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import BigMain from "./components/BigMain"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <BigMain />
+        </>
+    )
+}
